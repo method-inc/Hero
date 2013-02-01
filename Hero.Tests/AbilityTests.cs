94664fe49b9faed66ee90c09aa557dcd24282443
Eric LaForce <eric@skookum.com>
2013-02-01 08:10:36 -0500

    More infrastructure setup

---
+++ Hero.Tests/AbilityTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hero.Tests
+{
+    [TestClass]
+    public class AbilityTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
