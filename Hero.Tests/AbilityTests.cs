4ef7a672ed1285b33b44c9a6ff8094ffe21e5bec
Eric LaForce <eric@skookum.com>
2013-02-03 17:37:56 -0500

    Added events to ability registration

--- Hero.Tests/AbilityTests.cs
+++ Hero.Tests/AbilityTests.cs
@@ -5,12 +5,14 @@ namespace Hero.Tests
 {
     public class AbilityTests
     {
-        private Ability _ability;
+        private Ability _ability1;
+        private Ability _ability2;
 
         [SetUp]
         public void Initialize()
         {
-            _ability = new Ability("ability");
+            _ability1 = new Ability("Ability1");
+            _ability2 = new Ability("Ability2");
         }
 
         [Test]
@@ -22,7 +24,87 @@ namespace Hero.Tests
         [Test]
         public void TestAbilityHasNameProperty()
         {
-            Assert.AreEqual("ability", _ability.Name);
+            Assert.AreEqual("Ability1", _ability1.Name);
+        }
+
+        [Test]
+        public void TestAbilitysAreEqual()
+        {
+            Ability abilityOne = new Ability("Ability1");
+            Assert.AreEqual(abilityOne, _ability1);
+        }
+
+        [Test]
+        public void TestAbilitysEqualityOperator()
+        {
+            Ability abilityOne = new Ability("Ability1");
+            Assert.True(abilityOne == _ability1);
+        }
+
+        [Test]
+        public void TestAbilitysAreNotEqual()
+        {
+            Assert.AreNotEqual(_ability2, _ability1);
+        }
+
+        [Test]
+        public void TestAbilitysNotEqualOperator()
+        {
+            Assert.True(_ability2 != _ability1);
+        }
+
+        [Test]
+        public void TestAbilityReferenceEqualsTrue()
+        {
+            Assert.AreEqual(_ability1, _ability1);
+        }
+
+        [Test]
+        public void TestAbilityReferenceEqualsTrueWithEqualsMethod()
+        {
+            Assert.True(_ability1.Equals(_ability1));
+        }
+
+        [Test]
+        public void TestAbilityNullReferenceReturnsFalse()
+        {
+            Assert.False(_ability1.Equals(null));
+        }
+
+        [Test]
+        public void TestAbilityDoesNotEqualADifferentObject()
+        {
+            Assert.AreNotEqual("", _ability1);
+        }
+
+        [Test]
+        public void TestAbilityReferenceEqualsTrueWithEqualsMethodCastObject()
+        {
+            Assert.True(_ability1.Equals((object)_ability1));
+        }
+
+        [Test]
+        public void TestAbilityNullReferenceCastObjectReturnsFalse()
+        {
+            Assert.False(_ability1.Equals((object)null));
+        }
+
+        [Test]
+        public void TestAbilityDoesNotEqualADifferentObjectCastObject()
+        {
+            Assert.False(_ability1.Equals((object)""));
+        }
+
+        [Test]
+        public void TestAbilityHashCodeEquals()
+        {
+            Assert.AreEqual(_ability1.GetHashCode(), _ability1.GetHashCode());
+        }
+
+        [Test]
+        public void TestAbilityHashCodeNotEquals()
+        {
+            Assert.AreNotEqual(_ability1.GetHashCode(), _ability2.GetHashCode());
         }
     }
 }
--- Hero/Ability.cs
+++ Hero/Ability.cs
@@ -3,7 +3,7 @@ using DotNetStandard.Interfaces;
 
 namespace Hero
 {
-    public class Ability : INameable
+    public class Ability : INameable, IEquatable<Ability>
     {
         private readonly string _name;
         public string Name
@@ -17,6 +17,36 @@ namespace Hero
                 throw new ArgumentNullException("name");
 
             _name = name;
+        }
+
+        public bool Equals(Ability other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(_name, other._name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Ability) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode();
+        }
+
+        public static bool operator ==(Ability left, Ability right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Ability left, Ability right)
+        {
+            return !Equals(left, right);
         }
     }
 }
