713aa164c9506a23adb90a8a41c3331776aa404f
Eric LaForce <eric@skookum.com>
2013-03-26 10:03:29 -0400

    Added abilities controller and corresponding tests

---
+++ Hero.Configuration/AbilitiesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using Hero.Interfaces;
+using Hero.Services.Interfaces;
+
+namespace Hero.Configuration
+{
+    public class AbilitiesController : Controller
+    {
+        private readonly IAbilityAuthorizationService _abilityAuthorizationService;
+
+        public AbilitiesController(IAbilityAuthorizationService abilityAuthorizationService)
+        {
+            if (abilityAuthorizationService == null) 
+                throw new ArgumentNullException("abilityAuthorizationService");
+
+            _abilityAuthorizationService = abilityAuthorizationService;
+        }
+
+        public IEnumerable<Ability> GetAbilitiesForRole(string roleName)
+        {
+            return _abilityAuthorizationService.GetAbilitiesForRole(roleName);
+        }
+
+        public IEnumerable<Ability> GetAbilitiesForUser(string userName)
+        {
+            return _abilityAuthorizationService.GetAbilitiesForUser(userName);
+        }
+
+        public IEnumerable<IRole> GetRolesForUser(string userName)
+        {
+            return _abilityAuthorizationService.GetRolesForUser(userName);
+        }
+    }
+}
---
+++ Hero.Tests/AbilitiesControllerTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hero.Configuration;
+using Hero.Interfaces;
+using Hero.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace Hero.Tests
+{
+    public class AbilitiesControllerTest
+    {
+        private Mock<IAbilityAuthorizationService> _authorizationServiceMock;
+        private AbilitiesController _controller;
+        private List<Ability> _abilities;
+        private List<IRole> _roles;
+        private IUser _user;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _Setup();
+            _authorizationServiceMock = new Mock<IAbilityAuthorizationService>();
+            _controller = new AbilitiesController(_authorizationServiceMock.Object);
+        }
+
+        [Test]
+        public void TestControllerWithEmptyServiceThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AbilitiesController(null));
+        }
+
+        [Test]
+        public void TestGetAbilitiesForRole()
+        {
+            _authorizationServiceMock.Setup(service => service.GetAbilitiesForRole("Role1")).Returns(_abilities);
+            IEnumerable<Ability> abilitiesForRole = _controller.GetAbilitiesForRole("Role1");
+            Assert.True(abilitiesForRole.SequenceEqual(_abilities));
+        }
+
+        [Test]
+        public void TestGetAbilitiesForUser()
+        {
+            _authorizationServiceMock.Setup(service => service.GetAbilitiesForUser("User1")).Returns(_abilities);
+            IEnumerable<Ability> abilitiesForUser = _controller.GetAbilitiesForUser("User1");
+            Assert.True(abilitiesForUser.SequenceEqual(_abilities));
+        }
+
+        [Test]
+        public void TestGetRolesForUser()
+        {
+            _authorizationServiceMock.Setup(service => service.GetRolesForUser("User1")).Returns(_roles);
+            IEnumerable<IRole> rolesForUser = _controller.GetRolesForUser("User1");
+            Assert.True(rolesForUser.SequenceEqual(_roles));
+        }
+
+        private void _Setup()
+        {
+            Ability ability1 = new Ability("Ability1");
+            Ability ability2 = new Ability("Ability2");
+            Ability ability3 = new Ability("Ability3");
+            _abilities = new List<Ability> {ability1, ability2, ability3};
+
+            IRole role1 = new Role("Role1");
+            IRole role2 = new Role("Role2");
+            IRole role3 = new Role("Role3");
+            _roles = new List<IRole> {role1, role2, role3};
+
+            _user = new User("User1");
+        }
+
+    }
+}
