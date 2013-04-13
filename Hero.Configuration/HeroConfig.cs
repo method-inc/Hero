a07b88bd2bac90dd653cad1037c1a5751aeca9c1
Keith La Force <klaforce@skookum.com>
2013-04-13 10:15:34 -0400

    Removing the service injection from HeroConfig

--- Hero.Configuration/HeroConfig.cs
+++ Hero.Configuration/HeroConfig.cs
@@ -18,23 +18,20 @@ namespace Hero.Configuration
         /// <summary>
         /// Assign a set of abilities to a role.
         /// </summary>
-        /// <param name="authorizationService">Ability based authorization service that manages role and abilities. Assumed to be a single instance</param>
         /// <param name="role">The role that is to be inspected and if nescessary configured</param>
         /// <param name="ability">The ability to assign the role if nescessary</param>
-        public static void RegisterAbility(IAbilityAuthorizationService authorizationService, IRole role,
+        public static void RegisterAbility(IRole role,
                                            Ability ability)
         {
-            RegisterAbilities(authorizationService, role, new List<Ability> {ability});
+            RegisterAbilities(role, new List<Ability> {ability});
         }
 
         /// <summary>
         /// Assign a set of abilities to a role.
         /// </summary>
-        /// <param name="authorizationService">Ability based authorization service that manages role and abilities. Assumed to be a single instance</param>
         /// <param name="role">The role that is to be inspected and if nescessary configured</param>
         /// <param name="abilities">The abilities to assign the role if nescessary</param>
-        public static void RegisterAbilities(IAbilityAuthorizationService authorizationService, IRole role, 
-                                             IEnumerable<Ability> abilities)
+        public static void RegisterAbilities(IRole role, IEnumerable<Ability> abilities)
         {
             // This method is intended to be used from the Global.asax.cs or
             // similar. It should only be done from there to encourage a centralized
@@ -47,37 +44,31 @@ namespace Hero.Configuration
             // any method of configuring roles and abilities (i.e. pulling roles from the database,
             // or abilities through refection)
 
-            if (authorizationService == null)
-                throw new ArgumentNullException("authorizationService");
             if (role == null)
                 throw new ArgumentNullException("role");
             if (abilities == null)
                 throw new ArgumentNullException("abilities");
 
             foreach (Ability ability in abilities)
-                authorizationService.RegisterAbility(role, ability);
+                AuthorizationService.RegisterAbility(role, ability);
         }
 
         /// <summary>
         /// Unassign a set of abilities to a role.
         /// </summary>
-        /// <param name="authorizationService">Ability based authorization service that manages role and abilities. Assumed to be a single instance</param>
         /// <param name="role">The role that is to be inspected and if nescessary configured</param>
         /// <param name="ability">The abilities to assign the role if nescessary</param>
-        public static void UnregisterAbility(IAbilityAuthorizationService authorizationService, IRole role,
-                                               Ability ability)
+        public static void UnregisterAbility(IRole role, Ability ability)
         {
-            UnregisterAbilities(authorizationService, role, new List<Ability> {ability});
+            UnregisterAbilities(role, new List<Ability> {ability});
         }
 
         /// <summary>
         /// Unassign a set of abilities to a role.
         /// </summary>
-        /// <param name="authorizationService">Ability based authorization service that manages role and abilities. Assumed to be a single instance</param>
         /// <param name="role">The role that is to be inspected and if nescessary configured</param>
         /// <param name="abilities">The abilities to assign the role if nescessary</param>
-        public static void UnregisterAbilities(IAbilityAuthorizationService authorizationService, IRole role, 
-                                               IEnumerable<Ability> abilities)
+        public static void UnregisterAbilities(IRole role, IEnumerable<Ability> abilities)
         {
             // This method is intended to be used from the Global.asax.cs or
             // similar. It should only be done from there to encourage a centralized
@@ -90,15 +81,13 @@ namespace Hero.Configuration
             // any method of configuring roles and abilities (i.e. pulling roles from the database,
             // or abilities through refection)
 
-            if (authorizationService == null)
-                throw new ArgumentNullException("authorizationService");
             if (role == null)
                 throw new ArgumentNullException("role");
             if (abilities == null)
                 throw new ArgumentNullException("abilities");
 
             foreach (Ability ability in abilities)
-                authorizationService.UnregisterAbility(role, ability);
+                AuthorizationService.UnregisterAbility(role, ability);
         }
 
         /// <summary>
@@ -107,20 +96,17 @@ namespace Hero.Configuration
         /// <param name="authorizationService"></param>
         /// <param name="user"></param>
         /// <param name="role"></param>
-        public static void RegisterRole(IAbilityAuthorizationService authorizationService, IUser user,
-                                        IRole role)
+        public static void RegisterRole(IUser user, IRole role)
         {
-            RegisterRoles(authorizationService, user, new List<IRole> {role});
+            RegisterRoles(user, new List<IRole> {role});
         }
 
         /// <summary>
         /// Assign a set of roles to a user
         /// </summary>
-        /// <param name="authorizationService"></param>
         /// <param name="user"></param>
         /// <param name="roles"></param>
-        public static void RegisterRoles(IAbilityAuthorizationService authorizationService, IUser user, 
-                                         IEnumerable<IRole> roles)
+        public static void RegisterRoles(IUser user, IEnumerable<IRole> roles)
         {
             // This method is intended to be used from the Global.asax.cs or
             // similar. It should only be done from there to encourage a centralized
@@ -130,15 +116,13 @@ namespace Hero.Configuration
             // any method of configuring roles and abilities (i.e. pulling roles from the database,
             // or abilities through refection)
 
-            if (authorizationService == null)
-                throw new ArgumentNullException("authorizationService");
             if (user == null)
                 throw new ArgumentNullException("user");
             if (roles == null)
                 throw new ArgumentNullException("roles");
 
             foreach (IRole role in roles)
-                authorizationService.RegisterRole(user, role);
+                AuthorizationService.RegisterRole(user, role);
         }
 
         /// <summary>
@@ -147,20 +131,17 @@ namespace Hero.Configuration
         /// <param name="authorizationService"></param>
         /// <param name="user"></param>
         /// <param name="role"></param>
-        public static void UnregisterRole(IAbilityAuthorizationService authorizationService, IUser user,
-                                          IRole role)
+        public static void UnregisterRole(IUser user, IRole role)
         {
-            UnregisterRoles(authorizationService, user, new List<IRole> {role});
+            UnregisterRoles(user, new List<IRole> {role});
         }
 
         /// <summary>
         /// Unassign a set of roles to a user
         /// </summary>
-        /// <param name="authorizationService"></param>
         /// <param name="user"></param>
         /// <param name="roles"></param>
-        public static void UnregisterRoles(IAbilityAuthorizationService authorizationService, IUser user, 
-                                           IEnumerable<IRole> roles)
+        public static void UnregisterRoles(IUser user, IEnumerable<IRole> roles)
         {
             // This method is intended to be used from the Global.asax.cs or
             // similar. It should only be done from there to encourage a centralized
@@ -170,15 +151,13 @@ namespace Hero.Configuration
             // any method of configuring roles and abilities (i.e. pulling roles from the database,
             // or abilities through refection)
 
-            if (authorizationService == null)
-                throw new ArgumentNullException("authorizationService");
             if (user == null)
                 throw new ArgumentNullException("user");
             if (roles == null)
                 throw new ArgumentNullException("roles");
 
             foreach (IRole role in roles)
-                authorizationService.UnregisterRole(user, role);
+                AuthorizationService.UnregisterRole(user, role);
         }
     }
 }
