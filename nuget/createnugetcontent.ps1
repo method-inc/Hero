0ffab8fbd40bd7af2302893eac854ceb0ebb2796
Eric LaForce <eric@skookum.com>
2013-04-23 22:44:05 -0400

    Removed Hero_Config from App_Start

--- nuget/createnugetcontent.ps1
+++ nuget/createnugetcontent.ps1
@@ -1,7 +1,3 @@
-Copy-Item "$args\Hero.Configuration\HeroConfig.cs" "$args\nuget\Content\App_Start"
-Remove-Item "$args\nuget\Content\App_Start\HeroConfig.cs.pp"
-Rename-Item -Force "$args\nuget\Content\App_Start\HeroConfig.cs" "$args\nuget\Content\App_Start\HeroConfig.cs.pp"
-
 Copy-Item "$args\Hero.Frontend\AbilitiesController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\AbilitiesController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\AbilitiesController.cs" "$args\nuget\Content\Controllers\AbilitiesController.cs.pp"
@@ -13,12 +9,6 @@ Rename-Item -Force "$args\nuget\Content\Scripts\hero.js" "$args\nuget\Content\Sc
 Copy-Item "$args\Hero.Frontend\craft.min.js" "$args\nuget\Content\Scripts"
 Remove-Item "$args\nuget\Content\Scripts\craft.min.js.pp"
 Rename-Item -Force "$args\nuget\Content\Scripts\craft.min.js" "$args\nuget\Content\Scripts\craft.min.js.pp"
-
-$original_file = "$args\nuget\Content\App_Start\HeroConfig.cs.pp"
-$destination_file = "$args\nuget\Content\App_Start\HeroConfig.cs.pp"
-(Get-Content $original_file) | Foreach-Object {
-    $_ -replace 'namespace Hero.Configuration', 'namespace $rootnamespace$'
-    } | Set-Content $destination_file
     
 $original_file = "$args\nuget\Content\Controllers\AbilitiesController.cs.pp"
 $destination_file = "$args\nuget\Content\Controllers\AbilitiesController.cs.pp"
