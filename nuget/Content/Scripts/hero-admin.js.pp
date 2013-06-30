1a255d6b64734cd51668bf1b195aec6d7e35871d
Keith La Force <klaforce@skookum.com>
2013-06-30 18:04:45 -0400

    Adding backbone forms and css/images, setup for admin console.

---
+++ nuget/Content/Scripts/hero-admin.js.pp
@@ -0,0 +1,55 @@
+﻿//undefined is a parameter that nothing is passed into
+//to protect it from being overwritten by another library
+//in the global namespace.
+(function (heroAdmin, request, undefined) {
+    //private instance data
+    var heroOptions = {};
+    var instance = {};
+    var currentUser = {};
+
+    heroOptions.endpoint = "http://localhost/Authorization/";
+    instance.options = heroOptions;
+
+    //private functions
+    var extend = function (optionsToMerge) {
+        for (var i in optionsToMerge) {
+            if (heroOptions.hasOwnProperty(i)) {
+                heroOptions[i] = optionsToMerge[i];
+            }
+        }
+    };
+
+    /* getRolesForUser
+     * options.user: User object
+     * options.then: callback to perform action when the request completes successfully.  Parameters are (Role[])
+     * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
+     * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
+     */
+    heroAdmin.getRolesForUser = function (options) {
+        if (!options.user || !options.user.userName)
+            throw "You must provide a user to the options list.";
+        if (!options.then)
+            throw "You must provide a then callback to the options list.";
+        if (!options.fail)
+            throw "You must provide a fail callback to the options list.";
+
+        request.get(instance.options.endpoint + "GetRolesForUser/" + options.user.userName)
+            .then(function (resp) {
+                options.then(deserializeRoleList(resp));
+            })
+            .fail(function (err, msg) {
+                options.then(err, msg);
+            })
+            .always(function (resp) {
+                if (options.always)
+                    options.always(resp);
+            })
+            .async(options.async === undefined ? true : options.async)
+            .update();
+
+        return this;
+    };
+})(window.HeroAdmin = window.HeroAdmin || {}, Request);   //Request is a Craft object used for ajax requests and promises
+
+//no conflict mode for craft
+$.noConflict()
--- nuget/createnugetcontent.ps1
+++ nuget/createnugetcontent.ps1
@@ -1,26 +1,50 @@
-Copy-Item "$args\Hero.Frontend\AuthorizationController.cs" "$args\nuget\Content\Controllers"
+Copy-Item "$args\Hero.Frontend\Controllers\AuthorizationController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\AuthorizationController.cs" "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
 
-Copy-Item "$args\Hero.Frontend\AbilityController.cs" "$args\nuget\Content\Controllers"
+Copy-Item "$args\Hero.Frontend\Controllers\AbilityController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\AbilityController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\AbilityController.cs" "$args\nuget\Content\Controllers\AbilityController.cs.pp"
 
-Copy-Item "$args\Hero.Frontend\RoleController.cs" "$args\nuget\Content\Controllers"
+Copy-Item "$args\Hero.Frontend\Controllers\RoleController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\RoleController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\RoleController.cs" "$args\nuget\Content\Controllers\RoleController.cs.pp"
 
-Copy-Item "$args\Hero.Frontend\UserController.cs" "$args\nuget\Content\Controllers"
+Copy-Item "$args\Hero.Frontend\Controllers\UserController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\UserController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\UserController.cs" "$args\nuget\Content\Controllers\UserController.cs.pp"
 
-Copy-Item "$args\Hero.Frontend\hero.js" "$args\nuget\Content\Scripts"
+Copy-Item "$args\Hero.Frontend\Scripts\hero.js" "$args\nuget\Content\Scripts"
 Remove-Item "$args\nuget\Content\Scripts\hero.js.pp"
 Rename-Item -Force "$args\nuget\Content\Scripts\hero.js" "$args\nuget\Content\Scripts\hero.js.pp"
 
-Copy-Item "$args\Hero.Frontend\craft.min.js" "$args\nuget\Content\Scripts"
+Copy-Item "$args\Hero.Frontend\Scripts\hero-admin.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\hero-admin.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\hero-admin.js" "$args\nuget\Content\Scripts\hero-admin.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Scripts\craft.min.js" "$args\nuget\Content\Scripts"
 Remove-Item "$args\nuget\Content\Scripts\craft.min.js.pp"
 Rename-Item -Force "$args\nuget\Content\Scripts\craft.min.js" "$args\nuget\Content\Scripts\craft.min.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Scripts\backbone-min.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\backbone-min.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\backbone-min.js" "$args\nuget\Content\Scripts\backbone-min.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Scripts\backbone-forms.min.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\backbone-forms.min.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\backbone-forms.min.js" "$args\nuget\Content\Scripts\backbone-forms.min.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Css\metro-bootstrap.css" "$args\nuget\Content\Content"
+Remove-Item "$args\nuget\Content\Content\metro-bootstrap.css.pp"
+Rename-Item -Force "$args\nuget\Content\Content\metro-bootstrap.css" "$args\nuget\Content\Content\metro-bootstrap.css.pp"
+
+Copy-Item "$args\Hero.Frontend\Images\glyphicons-halflings-white.png" "$args\nuget\Content\Images"
+Remove-Item "$args\nuget\Content\Images\glyphicons-halflings-white.png.pp"
+Rename-Item -Force "$args\nuget\Content\Images\glyphicons-halflings-white.png" "$args\nuget\Content\Images\glyphicons-halflings-white.png.pp"
+
+Copy-Item "$args\Hero.Frontend\Images\glyphicons-halflings.png" "$args\nuget\Content\Images"
+Remove-Item "$args\nuget\Content\Images\glyphicons-halflings.png.pp"
+Rename-Item -Force "$args\nuget\Content\Images\glyphicons-halflings.png" "$args\nuget\Content\Images\glyphicons-halflings.png.pp"
 
 $original_file = "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
 $destination_file = "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
