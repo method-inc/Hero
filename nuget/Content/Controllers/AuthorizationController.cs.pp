024097175062978bf0925de73c437d6758318166
Keith La Force <klaforce@skookum.com>
2013-07-03 19:13:43 -0400

    Updating Authorization controller to new model.

--- nuget/Content/Controllers/AuthorizationController.cs.pp
+++ nuget/Content/Controllers/AuthorizationController.cs.pp
@@ -1,30 +1,15 @@
-using System.Collections.Generic;
-using System.Web.Mvc;
+using System.Web.Http;
 using Hero; //do not remove
 using Hero.Configuration;
 using Hero.Interfaces;
 
 namespace $rootnamespace$
 {
-    public class AuthorizationController : Controller
+    public class AuthorizationController : ApiController
     {
-        public JsonResult GetAbilitiesForUser(string id)
+        public IUser GetCurrentUser()
         {
-            return Json(HeroConfig.AuthorizationService.GetAbilitiesForUser(id), JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public JsonResult AuthorizeCurrentUser(string id, string user = null)
-        {
-            if(user == null)
-                user = HttpContext.User.Identity.Name;
-
-            return Json(HeroConfig.AuthorizationService.Authorize(user, id), JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetCurrentUser()
-        {
-            return Json(HttpContext.User.Identity.Name, JsonRequestBehavior.AllowGet);
+            return HeroConfig.AuthorizationService.GetUser(User.Identity.Name);
         }
     }
 }
