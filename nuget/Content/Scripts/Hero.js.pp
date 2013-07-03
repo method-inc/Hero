5e2494469a8b99b2e0767e8dbea82212911f568b
Keith La Force <klaforce@skookum.com>
2013-07-03 17:06:25 -0400

    Fixing some issues in the package and getting all the new files into nuget.

--- nuget/Content/Scripts/Hero.js.pp
+++ nuget/Content/Scripts/Hero.js.pp
@@ -1,326 +1,85 @@
-﻿(function (hero, request, undefined) {
-  //private instance data
+(function (hero, angular, undefined) {
   var heroOptions = {};
-  var instance = {};
-  var currentUser = {};
-  var baseurl = !window.location.origin ? window.location.protocol + "//" + window.location.host : window.location.origin;
+  
+  heroOptions.userListTemplatePath = "/Templates/user-list.html";
+  heroOptions.userEditTemplatePath = "/Templates/user-create.html";
+  heroOptions.userNewTemplatePath = "/Templates/user-create.html";
+  heroOptions.roleListTemplatePath = "/Templates/role-list.html";
+  heroOptions.roleEditTemplatePath = "/Templates/role-create.html";
+  heroOptions.roleNewTemplatePath = "/Templates/role-create.html";
+  heroOptions.abilityListTemplatePath = "/Templates/ability-list.html";
+  heroOptions.abilityEditTemplatePath = "/Templates/ability-create.html";
+  heroOptions.abilityNewTemplatePath = "/Templates/ability-create.html";
+  heroOptions.headerTemplatePath = "/Templates/header.html";
+  heroOptions.adminConsoleInjectionId = "admin-console";
+  heroOptions.apiBaseUrl = "api/";
 
-  heroOptions.endpoint = baseurl + "/Authorization/";
-  instance.options = heroOptions;
-
-  //private functions
-  var extend = function (optionsToMerge) {
-    for (var i in optionsToMerge) {
-      if (heroOptions.hasOwnProperty(i)) {
-        heroOptions[i] = optionsToMerge[i];
-      }
-    }
+  hero.configure = function (options) {
+    angular.extend(heroOptions, options);
+    return this;
   };
 
-  var deserializeAbilityList = function (abilityObjList) {
-    var retArray = [];
-    for (var index in abilityObjList) {
-      retArray.push(deserializeAbility(abilityObjList[index]));
-    }
-
-    return retArray;
+  hero.buildConsole = function() {
+    document.getElementsByTagName('html')[0].setAttribute('ng-app', 'heroApp');
+    document.getElementById(heroOptions.adminConsoleInjectionId).setAttribute('ng-view', '');
+    return this;
   };
 
-  var deserializeAbility = function (abilityObj) {
-    var ability = hero.Ability(abilityObj.Name, []);
+  hero.heroApp = angular.module('heroApp', ['restangular']);
 
-    for (var childIndex in abilityObj.Children) {
-      ability.children.push(new hero.Ability(abilityObj.Children[childIndex].Name, []));
-    }
-
-    return ability;
-  };
-
-  var deserializeRoleList = function (roleObjList) {
-    var retArray = [];
-    for (var index in roleObjList) {
-      retArray.push(deserializeRole(roleObjList[index]));
-    }
-
-    return retArray;
-  };
-
-  var deserializeRole = function (roleObj) {
-    return hero.Role(roleObj.Name);
-  };
-
-  /**
-    * Returns a list of abilities for the current user.
-    */
-  var intializeCurrentUser = function () {
-    hero.getCurrentUser(
-        {
-          then: function (userName) {
-            if (!userName) {
-              //create a fake user since this person is not authenticated
-              currentUser = hero.User("UnAuthenticated");
-              return;
-            }
-
-            currentUser = hero.User(userName);
-
-            //now that we have the current user, populate the abilitites.
-            hero.getAbilititesForUser({
-              user: currentUser,
-              then: function (abilities) {
-                currentUser.abilitites = abilities;
-              },
-              fail: function (err, msg) {
-                console.log(msg);
-              },
-              async: false
-            });
-          },
-          fail: function (err, msg) { console.log(msg); },
-          async: false
-        });
-  };
-
-  //Public Model Types
-  hero.User = function (name, abilitites) {
+  hero.heroApp.factory("heroOptions", function() {
     return {
-      userName: name,
-      abilitites: abilitites,
-      isAuthorized: function (ability) {
-        if (!ability || !ability.abilityName) {
-          throw "You must provide an ability to the isAuthorized function.";
-        }
+      getOptions: function() {
+        return heroOptions;
+      }
+    };
+  });
 
-        for (var currAbilityIndex in this.abilitites) {
-          if (this.abilitites[currAbilityIndex].abilityName === ability.abilityName) {
-            return true;
+  hero.heroApp.config(function($routeProvider, RestangularProvider) {
+    RestangularProvider.setBaseUrl(heroOptions.apiBaseUrl);
+    
+    $routeProvider
+      .when('/users', { templateUrl: heroOptions.userListTemplatePath, controller: 'UserListController' })
+      .when('/edituser/:userId', {
+        templateUrl: heroOptions.userEditTemplatePath,
+        controller: 'EditUserController',
+        resolve: {
+          user: function(Restangular, $route) {
+            return Restangular.one('user', $route.current.params.userId).get();
           }
         }
-
-        return false;
-      }
-    };
-  };
-
-  hero.Role = function (name) {
-    return { roleName: name };
-  };
-
-  hero.Ability = function (name, children) {
-    return { abilityName: name, children: children };
-  };
-
-  //public API (configuration)
-  hero.configure = function (options) {
-    extend(options);
-    instance.options = heroOptions;
-    intializeCurrentUser();
-    return this;
-  };
-
-  /**
-    * The registration can take a true object or a function as the second paramter
-    * If only an object and no function are passed, it will register itself with every function off the object
-    */
-  hero.registerAbility = function (ability, target, fn) {
-    if (!ability || !ability.abilityName)
-      throw "You must pass a Hero.Ability as the first parameter for registration.";
-    if (!target || !(target instanceof Object))
-      throw "You must pass an object as the target.";
-    if (!fn || !(fn instanceof Function)) {
-      //if they dont' pass a function assume they want every function on that object registerd
-      for (var potentialFunctionIndex in target) {
-        if (target[potentialFunctionIndex] instanceof Function) {
-          hero.registerAbility(ability, target, target[potentialFunctionIndex]);
+      })
+      .when('/newuser', { templateUrl: heroOptions.userNewTemplatePath, controller: 'NewUserController' })
+      .when('/roles', { templateUrl: heroOptions.roleListTemplatePath, controller: 'RoleListController' })
+      .when('/editrole/:roleId', {
+        templateUrl: heroOptions.roleEditTemplatePath,
+        controller: 'EditRoleController',
+        resolve: {
+          role: function(Restangular, $route) {
+            return Restangular.one('role', $route.current.params.roleId).get();
+          }
         }
-      }
-    }
+      })
+      .when('/newrole', { templateUrl: heroOptions.roleNewTemplatePath, controller: 'NewRoleController' })
+      .when('/abilities', { templateUrl: heroOptions.abilityListTemplatePath, controller: 'AbilityListController' })
+      .when('/editability/:abilityId', {
+        templateUrl: heroOptions.abilityEditTemplatePath,
+        controller: 'EditAbilityController',
+        resolve: {
+          ability: function(Restangular, $route) {
+            return Restangular.one('ability', $route.current.params.abilityId).get();
+          }
+        }
+      })
+      .when('/newability', { templateUrl: heroOptions.abilityNewTemplatePath, controller: 'NewAbilityController' })
+      .otherwise({ redirectTo: '/users' });
+  });
 
-    //if we are running registration the second time
-    //you need to use the original function.
-    if (fn.trueName) {
-      fn = fn.originalFunction;
-    }
+  hero.heroApp.controller('NavbarController', function($scope, $location) {
 
-    target[fn.name] = function () {
-      var that = this;
-      var thatArguments = arguments;
-
-      var isAuthorized = currentUser.isAuthorized(ability);
-
-      if (isAuthorized) {
-        return fn.apply(that, thatArguments);
-      } else {
-        return "UserNotAuthorized";
-      }
+    $scope.routeContains = function(routeName) {
+      return $location.path().indexOf(routeName) >= 0;
     };
 
-    //setup the option of re-registering
-    target[fn.name].trueName = fn.name;
-    target[fn.name].originalFunction = fn;
-
-    return this;
-  };
-
-  //public API (actions)
-
-  /* authorizeCurrentUser
-   * options.ability: Ability object
-   * options.then: callback to perform action when the request completes successfully.  Parameters are (string userName)
-   * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
-   * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
-   */
-  hero.getCurrentUser = function (options) {
-    if (!options.then)
-      throw "You must provide a then callback to the options list.";
-    if (!options.fail)
-      throw "You must provide a fail callback to the options list.";
-
-    request.get(instance.options.endpoint + "GetCurrentUser/")
-        .then(function (resp) {
-          //resp will be a boolean.
-          options.then(resp);
-        })
-        .fail(function (err, msg) {
-          options.then(err, msg);
-        })
-        .always(function (resp) {
-          if (options.always)
-            options.always(resp);
-        })
-        .async(options.async === undefined ? true : options.async)
-        .update();
-
-    return this;
-  };
-
-  /* authorizeCurrentUser
-   * options.ability: Ability object
-   * options.then: callback to perform action when the request completes successfully.  Parameters are (bool)
-   * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
-   * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
-   */
-  hero.authorizeCurrentUser = function (options) {
-    if (!options.ability || !options.ability.abilityName)
-      throw "You must provide a ability to the options list.";
-    if (!options.then)
-      throw "You must provide a then callback to the options list.";
-    if (!options.fail)
-      throw "You must provide a fail callback to the options list.";
-
-    request.get(instance.options.endpoint + "AuthorizeCurrentUser/" + options.ability.abilityName)
-        .then(function (resp) {
-          //resp will be a boolean.
-          options.then(resp);
-        })
-        .fail(function (err, msg) {
-          options.then(err, msg);
-        })
-        .always(function (resp) {
-          if (options.always)
-            options.always(resp);
-        })
-        .async(options.async === undefined ? true : options.async)
-        .update();
-
-    return this;
-  };
-
-  /* getAbilititesForUser
-   * options.user: User object
-   * options.then: callback to perform action when the request completes successfully.  Parameters are (Ability[])
-   * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
-   * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
-   */
-  hero.getAbilititesForUser = function (options) {
-
-    if (!options.user || !options.user.userName)
-      throw "You must provide a user to the options list.";
-    if (!options.then)
-      throw "You must provide a then callback to the options list.";
-    if (!options.fail)
-      throw "You must provide a fail callback to the options list.";
-
-    request.get(instance.options.endpoint + "GetAbilitiesForUser/" + options.user.userName)
-        .then(function (resp) {
-          //deserialize the ability objects
-          options.then(deserializeAbilityList(resp));
-        })
-        .fail(function (err, msg) {
-          options.then(err, msg);
-        })
-        .always(function (resp) {
-          if (options.always)
-            options.always(resp);
-        })
-        .async(options.async === undefined ? true : options.async)
-        .update();
-
-    return this;
-  };
-
-  /* getAbilitiesForRole
-   * options.role: Role object
-   * options.then: callback to perform action when the request completes successfully.  Parameters are (Ability[])
-   * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
-   * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
-   */
-  hero.getAbilitiesForRole = function (options) {
-    if (!options.role || !options.role.roleName)
-      throw "You must provide a role to the options list.";
-    if (!options.then)
-      throw "You must provide a then callback to the options list.";
-    if (!options.fail)
-      throw "You must provide a fail callback to the options list.";
-
-    request.get(instance.options.endpoint + "GetAbilitiesForRole/" + options.role.roleName)
-        .then(function (resp) {
-          options.then(deserializeAbilityList(resp));
-        })
-        .fail(function (err, msg) {
-          options.then(err, msg);
-        })
-        .always(function (resp) {
-          if (options.always)
-            options.always(resp);
-        })
-        .async(options.async === undefined ? true : options.async)
-        .update();
-
-    return this;
-  };
-
-  /* getRolesForUser
-   * options.user: User object
-   * options.then: callback to perform action when the request completes successfully.  Parameters are (Role[])
-   * options.fail: callback to perform an action on a request failure.  Parameters are (error, message)
-   * options.always (optional): callback to perform when the request is over (failure or success). Parameters are (response)
-   */
-  hero.getRolesForUser = function (options) {
-    if (!options.user || !options.user.userName)
-      throw "You must provide a user to the options list.";
-    if (!options.then)
-      throw "You must provide a then callback to the options list.";
-    if (!options.fail)
-      throw "You must provide a fail callback to the options list.";
-
-    request.get(instance.options.endpoint + "GetRolesForUser/" + options.user.userName)
-        .then(function (resp) {
-          options.then(deserializeRoleList(resp));
-        })
-        .fail(function (err, msg) {
-          options.then(err, msg);
-        })
-        .always(function (resp) {
-          if (options.always)
-            options.always(resp);
-        })
-        .async(options.async === undefined ? true : options.async)
-        .update();
-
-    return this;
-  };
-})(window.Hero = window.Hero || {}, Request);   //Request is a Craft object used for ajax requests and promises
-
-//no conflict mode for craft
-$.noConflict()
+  });
+})(window.Hero = window.Hero || {}, angular);
--- nuget/createnugetcontent.ps1
+++ nuget/createnugetcontent.ps1
@@ -1,3 +1,5 @@
+Write-Host $args
+
 Copy-Item "$args\Hero.Frontend\Controllers\AuthorizationController.cs" "$args\nuget\Content\Controllers"
 Remove-Item "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
 Rename-Item -Force "$args\nuget\Content\Controllers\AuthorizationController.cs" "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
@@ -18,13 +20,21 @@ Copy-Item "$args\Hero.Frontend\Scripts\hero.js" "$args\nuget\Content\Scripts"
 Remove-Item "$args\nuget\Content\Scripts\hero.js.pp"
 Rename-Item -Force "$args\nuget\Content\Scripts\hero.js" "$args\nuget\Content\Scripts\hero.js.pp"
 
-Copy-Item "$args\Hero.Frontend\Scripts\hero-admin.js" "$args\nuget\Content\Scripts"
-Remove-Item "$args\nuget\Content\Scripts\hero-admin.js.pp"
-Rename-Item -Force "$args\nuget\Content\Scripts\hero-admin.js" "$args\nuget\Content\Scripts\hero-admin.js.pp"
+Copy-Item "$args\Hero.Frontend\Scripts\hero-ability.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\hero-ability.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\hero-ability.js" "$args\nuget\Content\Scripts\hero-ability.js.pp"
 
-Copy-Item "$args\Hero.Frontend\Scripts\craft.min.js" "$args\nuget\Content\Scripts"
-Remove-Item "$args\nuget\Content\Scripts\craft.min.js.pp"
-Rename-Item -Force "$args\nuget\Content\Scripts\craft.min.js" "$args\nuget\Content\Scripts\craft.min.js.pp"
+Copy-Item "$args\Hero.Frontend\Scripts\hero-authorization.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\hero-authorization.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\hero-authorization.js" "$args\nuget\Content\Scripts\hero-authorization.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Scripts\hero-role.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\hero-role.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\hero-role.js" "$args\nuget\Content\Scripts\hero-role.js.pp"
+
+Copy-Item "$args\Hero.Frontend\Scripts\hero-user.js" "$args\nuget\Content\Scripts"
+Remove-Item "$args\nuget\Content\Scripts\hero-user.js.pp"
+Rename-Item -Force "$args\nuget\Content\Scripts\hero-user.js" "$args\nuget\Content\Scripts\hero-user.js.pp"
 
 Copy-Item "$args\Hero.Frontend\Scripts\restangular.js" "$args\nuget\Content\Scripts"
 Remove-Item "$args\nuget\Content\Scripts\restangular.js.pp"
@@ -41,6 +51,26 @@ Rename-Item -Force "$args\nuget\Content\Templates\user-create.html" "$args\nuget
 Copy-Item "$args\Hero.Frontend\Templates\user-list.html" "$args\nuget\Content\Templates"
 Remove-Item "$args\nuget\Content\Templates\user-list.html.pp"
 Rename-Item -Force "$args\nuget\Content\Templates\user-list.html" "$args\nuget\Content\Templates\user-list.html.pp"
+
+Copy-Item "$args\Hero.Frontend\Templates\role-create.html" "$args\nuget\Content\Templates"
+Remove-Item "$args\nuget\Content\Templates\role-create.html.pp"
+Rename-Item -Force "$args\nuget\Content\Templates\role-create.html" "$args\nuget\Content\Templates\role-create.html.pp"
+
+Copy-Item "$args\Hero.Frontend\Templates\role-list.html" "$args\nuget\Content\Templates"
+Remove-Item "$args\nuget\Content\Templates\role-list.html.pp"
+Rename-Item -Force "$args\nuget\Content\Templates\role-list.html" "$args\nuget\Content\Templates\role-list.html.pp"
+
+Copy-Item "$args\Hero.Frontend\Templates\ability-create.html" "$args\nuget\Content\Templates"
+Remove-Item "$args\nuget\Content\Templates\ability-create.html.pp"
+Rename-Item -Force "$args\nuget\Content\Templates\ability-create.html" "$args\nuget\Content\Templates\ability-create.html.pp"
+
+Copy-Item "$args\Hero.Frontend\Templates\ability-list.html" "$args\nuget\Content\Templates"
+Remove-Item "$args\nuget\Content\Templates\ability-list.html.pp"
+Rename-Item -Force "$args\nuget\Content\Templates\ability-list.html" "$args\nuget\Content\Templates\ability-list.html.pp"
+
+Copy-Item "$args\Hero.Frontend\Templates\header.html" "$args\nuget\Content\Templates"
+Remove-Item "$args\nuget\Content\Templates\header.html.pp"
+Rename-Item -Force "$args\nuget\Content\Templates\header.html" "$args\nuget\Content\Templates\header.html.pp"
 
 $original_file = "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
 $destination_file = "$args\nuget\Content\Controllers\AuthorizationController.cs.pp"
